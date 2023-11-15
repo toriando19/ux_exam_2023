99e5506f80f8b2392473fe2c0673e7622e4c7371
emmarodam <emmarodam@gmail.com>
2023-11-15 20:05:37 +0100

    Javascript

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
 // Change header background color
 let header = document.getElementById('site-header');
 
-document.addEventListener('scroll', function() {
-  
+document.addEventListener('scroll', function () {
+
   // Get the scroll position
   let scrollPos = window.pageYOffset;
-  
-  if ( scrollPos > 100 ) {
+
+  if (scrollPos > 100) {
     header.style.backgroundColor = "#001c55";
   } else {
     header.style.backgroundColor = "transparent";
@@ -14,16 +14,28 @@ document.addEventListener('scroll', function() {
 });
 
 // Accordion
-var acc = document.getElementsByClassName("accordion");
+let acc = document.getElementsByClassName("accordion");
 
-for (var i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+// Search bar visibility
+
+function removeClass() {
+  let searchContainer = document.getElementById('search-container');
+    searchContainer.classList.remove('hidden');
+}
+
+function addClass() {
+  let searchContainer = document.getElementById('search-container');
+    searchContainer.classList.add('hidden');
 }
