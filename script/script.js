0ec247f0d03908c0e123438b0faa69615ad16035
viktoriaandonovska <viktoria_andonovska@hotmail.com>
2023-11-16 00:29:31 +0100

    Check-out-changes

--- script/script.js
+++ script/script.js
@@ -39,3 +39,25 @@ function addClass() {
   let searchContainer = document.getElementById('search-container');
     searchContainer.classList.add('hidden');
 }
+
+
+
+// Check-out-form visibility ///////////////////////////////////////////////////////////////////////////
+
+//Skal fixes...
+
+document.getElementById('form').addEventListener('input', function () {
+  // Check if both name and email fields are filled
+  var name = document.getElementById('name').value.trim();
+  var email = document.getElementById('email').value.trim();
+
+  if (name !== '' && email !== '') {
+      // If both fields are filled, show the hidden content
+      document.getElementById('hiddenContent').style.display = 'block';
+  } else {
+      // If any field is empty, hide the content
+      document.getElementById('hiddenContent').style.display = 'none';
+  }
+});
+
+ /////////////////////////////////////////////////////////////////////////////////////////////////////////
