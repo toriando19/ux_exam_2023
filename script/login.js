6da0d0d509e3d819b30067e02c91291ff83fef62
josefinemaria97 <91532078+josefinemaria97@users.noreply.github.com>
2023-11-22 16:37:35 +0100

    ..

--- script/login.js
+++ script/login.js
@@ -1,39 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
     let signupBtn = document.getElementById("signupBtn");
-    let loginBtn = document.getElementById("loginBtn");
     let title = document.getElementById("title");
     let emailField = document.querySelector('input[type="email"]');
-    let passwordInput= document.getElementById("passwordInput");
+    let passwordInput = document.getElementById("passwordInput");
     let confirmPasswordField = document.getElementById("confirmPasswordField");
     let nameField = document.getElementById("nameField");
 
-   
-
-    loginBtn.addEventListener('click', function () {
-        confirmPasswordField.style.maxHeight = "0"; // Ensure it's hidden on login
-        title.innerHTML = "Login";
-        nameField.style.maxHeight = "0";
-        signupBtn.classList.add("disable");
-        loginBtn.classList.remove("disable");
-    });
-
-    signupBtn.addEventListener('click', function (event) {
-        event.preventDefault(); // Prevent form submission
-        console.log("Button clicked");
+    signupBtn.addEventListener('click', async function (event) {
+        event.preventDefault();
         nameField.style.maxHeight = "60px";
         confirmPasswordField.style.maxHeight = "60px"; // Show confirm password on sign-up
         title.innerHTML = "Sign up";
-        signupBtn.classList.remove("disable");
-        loginBtn.classList.add("disable");
-    
+
         let email = emailField.value;
         let password = passwordInput.value;
         let confirmPassword = confirmPasswordField.querySelector('input[type="password"]').value;
-    
+
         // Password validation logic
         let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+}{":;'?/>.<,])(?=.*[a-zA-Z0-9]).{8,20}$/;
         let isPasswordValid = passwordRegex.test(password);
-    
+
         if (password !== confirmPassword) {
             // Display an error for password mismatch
             alert("Passwords do not match");
@@ -43,11 +29,22 @@ document.addEventListener("DOMContentLoaded", function() {
             alert("Password must be between 8 and 20 characters, contain lowercase and uppercase letters, numbers, and special characters.");
             return;
         }
-    
-        // Continue with the sign-up process if everything is valid
-        // You can add your sign-up API call or further processing here
 
+        try {
+            // Simulating a successful signup for demonstration
+            // Replace this with your actual fetch to the server
+            // ...
+
+            // On successful signup, redirect to login.html
+            redirectToLogin();
+        } catch (error) {
+            console.error('Error:', error);
+        }
     });
-    
+
+    function redirectToLogin() {
+        // Redirect to login.html after successful signup
+        window.location.href = "login.html";
+    }
 });
 
